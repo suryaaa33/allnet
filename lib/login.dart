c87a0c760b383ca3e4b9aa709b862434b8d37d34
Dede <dbangtan38@gmail.com>
2024-12-03 11:55:09 +0700

    assesment2_suryandari

---
+++ lib/login.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'bottomnav.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login Page',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Demo LOGIN",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue,
+        iconTheme: IconThemeData(color: Colors.white),
+      ),
+      body: Container(
+        color: Colors.white, // Background body putih
+        margin: const EdgeInsets.all(15.0),
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                autofocus: true,
+                decoration: const InputDecoration(
+                  hintText: 'Masukkan nama',
+                  labelText: 'Nama:',
+                  icon: Icon(Icons.person),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 8.0),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Masukkan nomor HP',
+                  labelText: 'No. HP:',
+                  icon: Icon(Icons.phone),
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.phone,
+              ),
+              SizedBox(height: 8.0),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Masukkan Email',
+                  labelText: 'Email:',
+                  icon: Icon(Icons.email),
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.emailAddress,
+              ),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue, 
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius:
+                        BorderRadius.circular(4.0), 
+                  ),
+                  padding: EdgeInsets.symmetric(
+                      vertical: 11.0, horizontal: 17.0), // Padding tombol
+                ),
+                child: Text(
+                  'Validasi Data',
+                  style: TextStyle(fontSize: 16),
+                ),
+                onPressed: () {
+                  // Navigasi langsung ke MainPage tanpa validasi
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => MainPage()),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
